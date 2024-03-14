253906b3fd89e5e51669db3b1f839de01bd85a4e
Mahigavale <maheshgavale07@gmail.com>
2024-03-14 15:52:51 +0530

    the returning function

--- returningfunction.js
+++ returningfunction.js
@@ -6,8 +6,10 @@ return keyword is used to send something to the caller.
 
 function returnfunction(a,b=20)
 {
-  return a+b;
-  console.log("this is return function.");
+  return(a+b);
+ // console.log("this is return function.");
 }
 
-console.log(returnfunction(23,23));
+//console.log(returnfunction(23,23));
+let cv=returnfunction(40,40);
+console.log(cv);
