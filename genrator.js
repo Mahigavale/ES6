dafe716d8ec23c787c19606521d0f699d369c3ef
Mahigavale <maheshgavale07@gmail.com>
2024-03-14 17:48:28 +0530

    the generator function

--- genrator.js
+++ genrator.js
@@ -20,7 +20,7 @@ function* demo()
 var demo2=demo();
 
 
-// console.log(demo2.next());
+ console.log(demo2.next().value=123);
 // console.log(demo2.next());
 // console.log(demo2.next());
 // console.log(demo2.next());
@@ -34,27 +34,29 @@ var demo2=demo();
 // console.log(demo2.next().done);
 
 //console.log(demo2);
-for(i in demo2)
-{
-    console.log(i);
-}
+// for(i in demo2)
+// {
+//     console.log(i);
+// }
 
-for(i of demo2)
-{
-    console.log(i);
-}
+// for(i of demo2)
+// {
+//     console.log(i);
+// }
 
 
-console.log("arrayyyyyyyyyyyyyyyyyyy");
+// console.log("arrayyyyyyyyyyyyyyyyyyy");
 
-let arr=[10,20,30]
+// let arr=[10,20,30]
 
-for(let i of arr)
-{
-    console.log(i);
-}
+// for(let i of arr)
+// {
+//     console.log(i);
+// }
 
-for (let i in arr)
-{
-    console.log(i);
-}
+// for (let i in arr)
+// {
+//     console.log(i);
+// }
+
+
