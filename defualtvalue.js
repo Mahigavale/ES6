6788ae8c5489cb25ba98a3466881ad5a28184bfb
Mahigavale <maheshgavale07@gmail.com>
2024-03-14 15:19:24 +0530

    defualt value.
    this is the demo for default value in js.

---
+++ defualtvalue.js
@@ -0,0 +1,16 @@
+
+/*
+
+default value: given in the function definiton only.
+
+*/
+
+function demo(a,b=20)
+{
+    console.log(a);
+    console.log(b);
+}
+
+console.log(demo);
+demo(12);
+demo(12,12,12);
